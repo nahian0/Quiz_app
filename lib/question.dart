114f0175e159e8ded507d1dc54d4f47f2e6af9c3
Nahian Pathan <nahianpathan12@gmail.com>
2022-04-06 12:20:55 +0600

    added question file

--- lib/question.dart
+++ lib/question.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+
 // ignore: must_be_immutable
 class Question extends StatelessWidget {
-
   final String questiontext;
   Question(this.questiontext);
 
   // ignore: empty_constructor_bodies
   @override
   Widget build(BuildContext context) {
-    return Text(questiontext);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(top: 30, bottom: 30),
+      child: Text(
+        questiontext,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
